27b3774a48204c6b703d09d63aab11f033dc35cc
ajengfitria <ajengfitria80@gmail.com>
2021-07-24 12:46:01 +0700

    add profile link

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,7 +8,7 @@
         <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">{{Auth::user()->name}}</a>
+        <a href="{{route('users.view_profile', ['id' => Auth::user()->id])}}" class="d-block">{{Auth::user()->name}}</a>
       </div>
     </div>
 
