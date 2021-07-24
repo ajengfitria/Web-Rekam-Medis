2229fcbe75251a27aaa3d7499ba846dc1939aff2
ajengfitria <ajengfitria80@gmail.com>
2021-07-24 12:29:01 +0700

    change document structure

--- eHealth/storage/framework/views/9bd0059ba75d39dc54052693aec911deb3f0b16e.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-	<strong>Copyright &copy; 2020 <a href="#">eHealth</a>.</strong>
-	All rights reserved.
-	<div class="float-right d-none d-sm-inline-block">
-		<b>Version</b> 3.1.0-rc
-	</div>
-</footer><?php /**PATH C:\xampp\htdocs\rekam-medis\Web-Rekam-Medis\eHealth\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
